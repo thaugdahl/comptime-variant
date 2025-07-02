6dbc6ef7323875e139ddc1690780e1461793d8bc
Tor Andre Haugdahl <thaugdahl@gmail.com>
2025-07-02 02:56:13 +0200

    Remove old overload code

--- src/overload.hpp
+++ src/overload.hpp
@@ -15,28 +15,6 @@ struct overload_set : Fs... {
     overload_set(Fs &&...fs) : Fs(std::forward<Fs>(fs))... {}
 };
 
-// Pre C++-17
-// -----------------------------------------------
-// template <typename... Fns> struct overload_set;
-// template <typename F1, typename... Fs>
-// struct overload_set<F1, Fs...> : F1, overload_set<Fs...> {
-//   using F1::operator();
-//   using overload_set<Fs...>::operator();
-//
-//   overload_set(F1 &&f1, Fs &&...fs) noexcept(
-//       std::is_nothrow_move_constructible_v<F1> &&
-//       std::is_nothrow_move_constructible_v<overload_set<Fs...>>)
-//       : F1(std::forward<F1>(f1)), overload_set<Fs...>(std::forward<Fs>(fs)...) {
-//   }
-// };
-//
-// template <typename F1> struct overload_set<F1> : F1 {
-//   using F1::operator();
-//
-//   overload_set(F1 &&f1) noexcept(std::is_nothrow_move_constructible_v<F1>)
-//       : F1(std::forward<F1>(f1)) {}
-// };
-
 template <typename... Fs> overload_set<Fs...> make_overload(Fs &&...fs) {
   return overload_set<Fs...>(std::forward<Fs>(fs)...);
 }
