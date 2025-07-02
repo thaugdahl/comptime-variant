181f68d5fb86fd239653c65e889259e4716ec743
Tor Andre Haugdahl <thaugdahl@gmail.com>
2025-07-02 03:01:11 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,37 @@
 #include <functional>
 #include <iostream>
 #include <type_traits>
-#include "traits.hpp"
 #include "overload.hpp"
 #include "variant.hpp"
 
+
+int visit_pure() {
+    Variant<int, float, double> var;
+    var.set<int>(2);
+
+    auto &z = var;
+
+    auto overloads = make_overload(
+        [](const float &y) -> int { std::cout << "float " << y << "\n"; return 1; },
+        [](const double &y) -> int { std::cout << "double " << y << "\n"; return 1; },
+        [](const int &k) -> int { std::cout << "int " << k << "\n"; return k;});
+
+    return z.visit(overloads);
+}
+
+int visit_mutating() {
+    Variant<int, float, double> var;
+    var.set<int>(2);
+
+    auto &z = var;
+
+    auto overloads = make_overload(
+        [](float &y) -> int { std::cout << "float " << y << "\n"; return 1; },
+        [](double &y) -> int { std::cout << "double " << y << "\n"; return 1; },
+        [](int &k) -> int { std::cout << "int " << k++ << "\n"; return ++k;});
+
+    return var.visit(overloads);
+}
 
 
 int main(int argc, char *argv[]) {
@@ -15,24 +42,12 @@ int main(int argc, char *argv[]) {
 
     static_assert(TypeIndex<int, float, int, double>::value == 1 && "Int at 0");
 
-    Variant<int, float, double> var;
+    int pure = visit_pure();
+    int mutating = visit_mutating();
 
-    var.set<int>(2);
+    std::cout << "Pure : " << pure << "\n";
+    std::cout << "Mutating : " << mutating << "\n";
 
-    auto overloads = make_overload(
-        [](float &y) -> int { std::cout << "float " << y << "\n"; return 1; },
-        [](double &y) -> int { std::cout << "double " << y << "\n"; return 1; },
-        [](int &k) -> int { std::cout << "int " << k++ << "\n"; return 2;});
-
-
-    const auto &z = var;
-    auto x = var.visit(overloads);
-
-    (void)var.visit([](auto &c) { std::cout << "printing " <<  c << "\n"; } );
-
-    std::cout << x << "\n";
-
-    std::cout << "Hello world\n";
 
     return 0;
 }
